3b0a6295855b020ab5570cb4007a55bf4f88f2ad
dbojdo <daniel@bojdo.eu>
2023-03-30 08:46:01 +0100

    [upd] doctrine/collections newer versions compatibility

--- tests/autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-$loader = include __DIR__ . '/../vendor/autoload.php';
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader([$loader, 'loadClass']);
-\Doctrine\Common\Annotations\AnnotationRegistry::registerAutoloadNamespace(
-    "JMS\\Serializer\\Annotation",
-    __DIR__ . '/../vendor/jms/serializer/src/JMS/Serializer/Annotation'
-);
